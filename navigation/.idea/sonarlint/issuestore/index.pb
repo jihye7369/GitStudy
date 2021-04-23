b209d738e08c52734c40812b9d1f2ba2de98c923
ori <jihye7369@doinglab.com>
2021-04-23 11:36:30 +0900

    버전변경

--- navigation/.idea/sonarlint/issuestore/index.pb
+++ navigation/.idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-Y
-)app/src/main/res/layout/activity_main.xml,7/6/76b07e40f24cce093536731477540f12e7da3cb6
 h
 8app/src/main/java/com/example/navigation/MainActivity.kt,e/6/e6d2c159e845667a32f21096aa7264b235505e60
--- navigation/app/src/main/java/com/example/navigation/MainActivity.kt
+++ navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvHello : TextView = findViewById(R.id.tv_hello)
+
+        Log.i("문자", tvHello.toString())
     }
 }
